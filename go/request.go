1b509f67780946fe12d1df0cf147dfe4fe72c3c6
Stefan Fransen <s.fransen@xeniox.nl>
2019-05-20 14:32:05 +0200

    Added more request types

--- go/request.go
+++ go/request.go
@@ -1,11 +1,16 @@
 package ubersmith
 
 import (
+  "mime/multipart"
+  "path/filepath"
   "encoding/json"
   "io/ioutil"
   "net/http"
   "net/url"
+  "bytes"
   "fmt"
+  "os"
+  "io"
 )
 
 type Response struct {
@@ -45,6 +50,90 @@ func (c *Client) Request(method string, params url.Values) (*Response, error) { 
 	if err != nil {
 		return nil, err
 	}
+
+	bytes, err := c.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+  var res Response
+  err = json.Unmarshal(bytes, &res)
+  if err != nil {
+		return nil, err
+	}
+
+  if !res.Status {
+    return nil, fmt.Errorf("Error %d, %s", res.ErrorCode, res.ErrorMessage)
+  }
+
+  return &res, nil
+}
+
+func (c *Client) PostRequest(method string, params url.Values) (*Response, error) { //params map[string][]string
+  m := url.Values{}
+  m.Add("method", method)
+  c.URL.RawQuery = m.Encode()
+
+  body := bytes.NewBufferString(params.Encode())
+
+  req, err := http.NewRequest("POST", c.URL.String(), body)
+	if err != nil {
+		return nil, err
+	}
+
+  req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	bytes, err := c.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+  var res Response
+  err = json.Unmarshal(bytes, &res)
+  if err != nil {
+		return nil, err
+	}
+
+  if !res.Status {
+    return nil, fmt.Errorf("Error %d, %s", res.ErrorCode, res.ErrorMessage)
+  }
+
+  return &res, nil
+}
+
+func (c *Client) Upload(method string, params url.Values, uploadParam string, uploadPath string) (*Response, error) { //params map[string][]string
+  file, err := os.Open(uploadPath)
+  if err != nil {
+      return nil, err
+  }
+  defer file.Close()
+
+  body := &bytes.Buffer{}
+  writer := multipart.NewWriter(body)
+  part, err := writer.CreateFormFile(uploadParam, filepath.Base(uploadPath))
+  if err != nil {
+      return nil, err
+  }
+
+  _, err = io.Copy(part, file)
+  if err != nil {
+      return nil, err
+  }
+
+  err = writer.Close()
+  if err != nil {
+      return nil, err
+  }
+
+  params.Set("method", method)
+  c.URL.RawQuery = params.Encode()
+
+  req, err := http.NewRequest("POST", c.URL.String(), body)
+	if err != nil {
+		return nil, err
+	}
+
+  req.Header.Set("Content-Type", writer.FormDataContentType())
 
 	bytes, err := c.doRequest(req)
 	if err != nil {
